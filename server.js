845570add3616b15f47469d0e01e4fd9e9c89453
Caitlin Ash <caitlinba27@gmail.com>
2023-09-09 20:47:12 -0500

    Changed connection format so that it is all in server.js file

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 // require express 
 const express = require('express');
-// require sequelize module from connection.js
-const sequelize = require('./config/connection.js');
+// import and require mysql2
+const mysql = require('mysql');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
-sequelize.sync().then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: '127.0.0.1',
+        // MySQL username
+        user: 'root',
+        // add MySQL password here
+        password: 'happycoding',
+        database: 'employees_db'
+      },
+      console.log(`Connected to the employees_db database.`)
+);
+
+// ADD ROUTES HERE
+
+// request not found
+app.use((req, res) => {
+    res.status(404).end();
+});
+// request successful
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
