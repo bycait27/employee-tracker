9f1bebc44c1911005e667479267704211540abc6
Caitlin Ash <caitlinba27@gmail.com>
2023-09-09 20:05:53 -0500

    Added server to view database via connection.js

---
+++ server.js
@@ -0,0 +1,14 @@
+// require express 
+const express = require('express');
+// require sequelize module from connection.js
+const sequelize = require('./config/connection.js');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extedned: true }));
+
+sequelize.sync().then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
