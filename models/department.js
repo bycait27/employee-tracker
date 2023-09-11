5193881c87cf9b09298edb286e695185190eeedd
Caitlin Ash <caitlinba27@gmail.com>
2023-09-11 17:06:50 -0500

    Rewrote department.js file to fit with mysql2

--- models/department.js
+++ models/department.js
@@ -1,9 +1,3 @@
-// require connection
-// const connection = require('../config/connection.js');
-// require sequelize
-// const sequelize = require('/Users/caitlinash/Desktop/coding-challenges/employee-tracker/config/connection.js');
-// const { Model, DataTypes } = require('sequelize');
-
 // require mysql2
 const mysql = require('mysql2');
 
@@ -11,81 +5,67 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: 'haooycoding',
+    password: 'happycoding',
     database: 'employees_db',
     connectionLimit: 10, // adjust the connection limit as needed
   });
 
-// class for department
-class Department extends Model {
-    // constructor(sequelize) {
-    //     this.sequelize = sequelize;
-    // }
-    // method to get all departments
-    async getAllDepartments() {
-        try {
-            const sql = 'SELECT * FROM department';
-            const [rows] = await connection.query(sql);
-            return rows;
-        } catch (error) {
-            console.log('Error in retrieving departments:', error);
-            throw error;
-        }
+    // function to get all departments
+    function getAllDepartments() {
+        return new Promise((resolve, reject) => {
+            pool.query('SELECT * FROM department', (err, results) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(results);
+              }
+            });
+          });
     }
-    // method to add a new departments
-    async addDepartment(department) {
-        try {
-            const sql = 'INSERT INTO department (name) VALUES (?)';
-            const [result] = await connection.query(sql, [department.name]);
-            return result.insertId;
-        } catch (error) {
-            console.log('Error in adding department:', error);
-            throw error;
-        }
-    }
-    // method to update a department
-    async updateDepartment(department) {
-        try {
-            const sql = 'UPDATE department SET name = ? WHERE id = ?';
-            await connection.query(sql, [department.name, department.id]);
-        } catch (error) {
-            console.log('Error updating department:', error);
-            throw error;
-        }
-    }
-    // method to delete a department
-    async deleteDepartment(departmentId) {
-        try {
-            const sql = 'DELETE FROM department WHERE id = ?';
-            await connection.query(sql,[departmentId]);
-        } catch (error) {
-            console.log('Error deleting department:', error);
-            throw error;
-        }
-    }  
-} 
 
-Department.init(
-    {
-        // define columns and datatypes for the table here
-          id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-          },
-          name: {
-            type: DataTypes.STRING(30),
-            allowNull: false,
-          }
-    },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'Department'
+    // function to add a department
+    function addDepartment(name) {
+        return new Promise((resolve, reject) => {
+        pool.query('INSERT INTO department (name) VALUES (?)', [name], (err, results) => {
+            if (err) {
+            reject(err);
+            } else {
+            resolve(results);
+            }
+        });
+        });
     }
-);
+  
+    // function to update a department
+    function updateDepartment(id, name) {
+        return new Promise((resolve, reject) => {
+        pool.query('UPDATE department SET name = ? WHERE id = ?', [name, id], (err, results) => {
+            if (err) {
+            reject(err);
+            } else {
+            resolve(results);
+            }
+        });
+        });
+    }
+    
+    // function to delete a department
+    function deleteDepartment(id) {
+        return new Promise((resolve, reject) => {
+        pool.query('DELETE FROM department WHERE id = ?', [id], (err, results) => {
+            if (err) {
+            reject(err);
+            } else {
+            resolve(results);
+            }
+        });
+        });
+    }
 
-// export Department class 
-module.exports = Department;
+// export all functions
+module.exports = {
+    getAllDepartments,
+    addDepartment,
+    updateDepartment,
+    deleteDepartment,
+};
